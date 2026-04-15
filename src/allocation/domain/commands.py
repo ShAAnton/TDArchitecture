c62d0fb07e93c79854c1d4df7960d91b63ccc1ec
ShAAnton <gmpochta1@gmail.com>
2026-04-15 13:13:00 +0300

    Detach command and event handlers

---
+++ src/allocation/domain/commands.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from typing import Optional
+from datetime import date
+
+
+class Command:
+    pass
+
+@dataclass
+class Allocate(Command):
+    order_id: str
+    sku: str
+    quantity: int
+
+@dataclass
+class CreateBatch(Command):
+    reference: str
+    sku: str
+    quantity: int
+    eta: Optional[date] = None
+
+@dataclass
+class ChangeBatchQuantity(Command):
+    reference: str
+    quantity: int
+
+@dataclass
+class Deallocate(Command):
+    order_id: str
+    sku: str
+    quantity: int
--- src/allocation/service_layer/message_bus.py
+++ src/allocation/service_layer/message_bus.py
@@ -1,8 +1,10 @@
-from typing import Dict, Type, List, Callable
-import allocation.domain.events as events
+from typing import Dict, Type, List, Callable, Union
+from allocation.domain import events, commands
 import allocation.service_layer.handlers
 from allocation.service_layer.unit_of_work import AbstractionUnitOfWork
+import logging
 
+Message = Union[events.Event, commands.Command]
 
 class AbstractionMessageBus:
     HANDLERS: Dict[Type[events.Event], List[Callable]]
@@ -10,24 +12,60 @@ class AbstractionMessageBus:
     def __init__(self, uow: AbstractionUnitOfWork):
         self.uow = uow
 
-    def handle(self, event: events.Event):
-        results, queue = list(), list()
-        queue = [event]
+    def handle(self, message: Message):
+        results, queue = list(), [event]
         while queue:
-            event = queue.pop(0)
-            for handler in self.HANDLERS[type(event)]:
-                results.append(handler(event, self.uow))
-                queue.extend(self.uow.collect_new_events())
+            message = queue.pop(0)
+            if isinstance(message, events.Event):
+                self.handle_event(event, queue, self.uow)
+            elif isinstance(message, commands.Command):
+                cmd_result = self.handle_command(message, queue, uow=self.uow)
+            else:
+                raise TypeError(f'{message} was not an Event or Command')
         return results
+
+    def handle_event(
+                self,
+                event: events.Event,
+                queue: List[Message],
+                uow: AbstractionUnitOfWork
+            ):
+        for handler in self.EVENT_HANDLERS[type(event)]:
+            try:
+                logger.debug('handling event %s with handler %s', event, handler)
+                handler(event, uow=uow)
+                queue.extend(uow.collect_new_events())
+            except Exception:
+                logger.exception('Exception handling event %s', event)
+                continue
+
+
+    def handle_command(
+            self,
+            command: commands.Command,
+            queue: List[Message],
+            uow: AbstractionUnitOfWork
+    ):
+        logger.debug('handling command %s', command)
+        try:
+            handler = self.COMMAND_HANDLERS[type(command)]
+            result = handler(command, uow=uow)
+            queue.extend(uow.collect_new_events())
+            return result
+        except Exception:
+            logger.exception('Exception handling command %s', command)
+            raise
 
 
 class MessageBus(AbstractionMessageBus):
-    HANDLERS = {
+    EVENT_HANDLERS = {
         events.OutOfStock: [allocation.service_layer.handlers.send_out_of_stock_notification],
-        events.BatchCreated: [allocation.service_layer.handlers.add_batch],
-        events.AllocationRequired: [allocation.service_layer.handlers.allocate],
-        events.DeallocationRequired: [allocation.service_layer.handlers.deallocate],
-        events.BatchQuantityChanged: [allocation.service_layer.handlers.change_batch_quantity]
+    }
+    COMMAND_HANDLERS = {
+        commands.CreateBatch: [allocation.service_layer.handlers.add_batch],
+        commands.Allocate: [allocation.service_layer.handlers.allocate],
+        commands.Deallocate: [allocation.service_layer.handlers.deallocate],
+        commands.ChangeBatchQuantity: [allocation.service_layer.handlers.change_batch_quantity]
     }
 
 
