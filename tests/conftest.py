3039a727e1b25ebb9df524ef4652c1e1c207b30b
ShAAnton <gmpochta1@gmail.com>
2026-05-01 13:47:23 +0300

    redis fixture becomes module level

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,7 +15,7 @@ from src.allocation import config
 from src.allocation.entrypoints.event_channels import ChannelEventConsumerOnline, ChannelEventConsumerPing
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def in_memory_db():
     engine = create_engine("sqlite:///:memory:")
     orm.metadata.create_all(engine)
@@ -86,7 +86,7 @@ def restart_api():
     wait_for_webapp_to_come_up()
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def restart_redis_pubsub():
     wait_for_redis_to_come_up()
     if not shutil.which("docker-compose"):
