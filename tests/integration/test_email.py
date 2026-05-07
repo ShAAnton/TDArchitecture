ee8f68e576f2b0465377aa290e3d02c002f7819f
ShAAnton <gmpochta1@gmail.com>
2026-05-07 10:27:20 +0300

    Add test_email.py

---
+++ tests/integration/test_email.py
@@ -0,0 +1,38 @@
+from allocation import bootstrap
+from allocation.adapters.orm import clear_mappers
+from allocation.service_layer import unit_of_work
+from allocation.adapters import notifications
+from allocation.domain import commands
+from allocation import config
+from ..random_refs import random_sku
+import requests
+import pytest
+
+
+
+@pytest.fixture
+def mbus(sqlite_session_factory):
+    mbus = bootstrap.bootstrap(
+        start_orm=True,
+        uow=unit_of_work.SqlAlchemyUnitOfWork(sqlite_session_factory),
+        notifications_=notifications.EmailNotification(),
+        publish=lambda *args: None,
+    )
+    yield mbus
+    clear_mappers()
+
+
+def get_email_from_mailhog(sku):
+    host, port = map(config.get_email_host_and_port().get, ['host', 'http_port'])
+    all_emails = requests.get(f'http://{host}:{port}/api/v2/messages').json()
+    return next(m for m in all_emails['items'] if sku in str(m))
+
+
+def test_out_of_stock_email(mbus):
+    sku = random_sku()
+    mbus.handle(commands.CreateBatch('batch1', sku, 9, None))
+    mbus.handle(commands.Allocate('order1', sku, 10))
+    email = get_email_from_mailhog(sku)
+    assert email['Raw']['From'] == 'allocations@example.com'
+    assert email['Raw']['To'] == ['stock@made.com']
+    assert f'Out of stock for {sku}' in email['Raw']['Data']
